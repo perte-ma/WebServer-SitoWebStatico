02e777d84792b48fed7793f231045b276eb011f0
Marco Pertegato <marco.pertegato@studio.unibo.it>
2025-06-01 18:18:02 +0200

    Added Relazione_Tecnica and Error404
    Added Relazione_Tecnica and handled the error 404 case returning to the client a message through an html page

--- TCP_Server.py
+++ TCP_Server.py
@@ -5,11 +5,24 @@ import socketserver
 class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.send_response(302)
+            self.send_response(301)
             self.send_header('Location', '/www/index.html')
             self.end_headers()
         else:
             super().do_GET()
+    def send_error(self, code, message=None, explain=None):
+        if code == 404:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"""\
+            <html>
+              <head><title>404 Not Found</title></head>
+              <body><h1>404 Not Found</h1></body>
+            </html>
+            """)
+        else:
+            super().send_error(code, message, explain)
 
 server = socketserver.ThreadingTCPServer(('127.0.0.1',8080), MyHTTPRequestHandler )
 
@@ -27,7 +40,7 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 try:
-    print(f"Server pronto su http://127.0.0.1:8080 ...")
+    print(f"Server ready to serve on http://127.0.0.1:8080 ...")
     server.serve_forever()
 except KeyboardInterrupt:
   pass
